235bcc81c51dd2653d5b306153e3ba21ebe55719
Malachi-Holden <malachi.coqui@gmail.com>
2022-09-04 16:57:54 -0500

    Hello World

---
+++ app/src/main/java/com/example/betterspotify/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.betterspotify
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.betterspotify.ui.theme.BetterSpotifyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BetterSpotifyTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BetterSpotifyTheme {
+        Greeting("Android")
+    }
+}
