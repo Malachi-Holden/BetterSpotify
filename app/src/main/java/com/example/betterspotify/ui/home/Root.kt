b6542b447561e7707d3d754e1782efe917432c31
Malachi-Holden <malachi.coqui@gmail.com>
2022-09-05 15:58:35 -0500

    Persist viewmodel

--- app/src/main/java/com/example/betterspotify/ui/home/Root.kt
+++ app/src/main/java/com/example/betterspotify/ui/home/Root.kt
@@ -1,22 +1,25 @@
 package com.example.betterspotify.ui.home
 
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.betterspotify.ui.Home
 import com.example.betterspotify.ui.item.ItemView
+import com.example.betterspotify.viewModel.HomeViewModel
 
 @Composable
 fun Root(){
     val navController = rememberNavController()
+    val viewModel: HomeViewModel = viewModel()
     NavHost(navController = navController, startDestination = "Home"){
         composable("Home"){
-            Home(navController)
+            Home(navController, viewModel)
         }
         composable("Item/{itemId}") { backStackEntry ->
-            ItemView(id = backStackEntry.arguments?.getString("itemId"))
+            ItemView(id = backStackEntry.arguments?.getString("itemId"), viewModel)
         }
     }
 }
